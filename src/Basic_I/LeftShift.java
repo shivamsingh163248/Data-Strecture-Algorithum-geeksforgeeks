01b35363a60b65dcadd2d864002c680a2f00cdd2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-02-15 20:07:02 +0530

    learning the right shif operator

--- src/Basic_I/LeftShift.java
+++ src/Basic_I/LeftShift.java
@@ -8,6 +8,9 @@ public class LeftShift {
 		// creating the left shift 
 		System.out.println(5>>1);
 		// learning the right and left shift operator 
+		// NOW WE ARAE THE WRITING THE RIGHT SHIFT OPERATOR 
+		System.out.println(5<<1);
+		// check what will be the ans 
 		
 
 	}
