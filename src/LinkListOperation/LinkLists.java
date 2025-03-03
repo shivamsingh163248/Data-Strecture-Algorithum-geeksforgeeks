98120e30196e2dc298aa28b36c00118a0d909ef9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-04 03:28:11 +0530

    Linklist operation

--- src/LinkListOperation/LinkLists.java
+++ src/LinkListOperation/LinkLists.java
@@ -5,19 +5,31 @@ public class LinkLists {
 	// creating the node head 
 	Node<Integer>head ;
 	// creating the insertion of the node 
-// public void insert(int data) { 
-//	 // checking the head if the null the link the node 
-//	  if (head == null) {
-//		// creating the new node and the set as the head 
-//		  head = new Node<Integer>(data);
-//	}else {
-//		// find the last element
-//		Node<Integer>Nodes  = head ; 
-//		while (Nodes.next != null) {
-//			Nodes = Nodes.next ; 
-//		}
-//	}
-//	  
-//}
+ public void insert(int data) { 
+	 // checking the head if the null the link the node 
+	  if (head == null) {
+		// creating the new node and the set as the head 
+		  head = new Node<Integer>(data);
+	}else {
+		// find the last element
+		Node<Integer>Nodes  = head ; 
+		while (Nodes.next != null) {
+			Nodes = Nodes.next ; 
+		}
+	}
+	  
+}
+ 
+ 
+ // creating the print  
+ public void print() {
+	 
+	 // creating the while loop 
+	 Node<Integer>Data = head ; 
+	 while (Data.next != null) {
+		System.out.println(Data.deta);
+		Data = Data.next ; 
+	}
+ }
 
 }
