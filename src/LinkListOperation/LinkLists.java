9fbe20a64014c11806f36aea8b21727ad8e3c159
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-04 03:21:16 +0530

    updating liklist operation

---
+++ src/LinkListOperation/LinkLists.java
@@ -0,0 +1,23 @@
+package LinkListOperation;
+
+
+public class LinkLists {
+	// creating the node head 
+	Node<Integer>head ;
+	// creating the insertion of the node 
+// public void insert(int data) { 
+//	 // checking the head if the null the link the node 
+//	  if (head == null) {
+//		// creating the new node and the set as the head 
+//		  head = new Node<Integer>(data);
+//	}else {
+//		// find the last element
+//		Node<Integer>Nodes  = head ; 
+//		while (Nodes.next != null) {
+//			Nodes = Nodes.next ; 
+//		}
+//	}
+//	  
+//}
+
+}
---
+++ src/LinkListOperation/Node.java
@@ -0,0 +1,13 @@
+package LinkListOperation;
+
+public class Node<T>{
+	
+	T deta ; 
+	Node<T>next ; 
+	
+	// creating the constructor 
+	public Node(T data) {
+		this.deta = data ; 
+	}
+
+}
