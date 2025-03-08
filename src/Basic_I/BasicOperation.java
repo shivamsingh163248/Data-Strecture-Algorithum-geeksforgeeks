047a8a41a8689ee5fc7a29f06922865d915e7c87
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-08 11:56:16 +0530

    updating

---
+++ src/Basic_I/BasicOperation.java
@@ -0,0 +1,11 @@
+package Basic_I;
+
+public class BasicOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double ans = 5.0/9 ; 
+       System.out.println(ans );
+	}
+
+}
---
+++ src/Basic_I/farenhight.java
@@ -0,0 +1,10 @@
+package Basic_I;
+
+public class farenhight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
