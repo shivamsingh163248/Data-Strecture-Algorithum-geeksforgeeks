c872a23c07a9b0b08d9b647d9df3a9a2bdd51e36
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-14 01:15:00 +0530

    updating the  values

--- src/BinnerySearch/Binneary_Search.java
+++ src/BinnerySearch/Binneary_Search.java
@@ -1,12 +1,26 @@
 package BinnerySearch;
 
+import java.util.Scanner;
+
 public class Binneary_Search {
 	
 	// Creating the function of the liner search 
 	// taking the input from the user 
 	public static <T>T[] taking_input(){
-		
-		T[]arr = new ; 
+		return null;
+	}
+	public static int[] user_taking_input() {
+		System.out.println("enter the size of the arrys ");
+		Scanner input = new Scanner(System.in) ; 
+		int n = input.nextInt() ; 
+		// creating the loop 
+		// creating the new arrays 
+		int[]arr = new int[n] ; 
+		for (int i = 0; i < arr.length; i++) {
+			int val = input.nextInt() ; 
+			arr[i] = val ; 
+			
+		}
 		return arr ; 
 	}
 
