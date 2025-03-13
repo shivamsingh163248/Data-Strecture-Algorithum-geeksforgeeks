c1f49610cc7ea73282e98e14fb2cd7f8946568c3
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-13 14:33:35 +0530

    update

--- src/Basic_I/farenhight.java
+++ src/Basic_I/farenhight.java
@@ -1,10 +1,26 @@
 package Basic_I;
 
+import java.util.Scanner;
+
 public class farenhight {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in) ; 
+		// taking the three input 
+		 int start = input.nextInt() ; 
+		 int end = input.nextInt() ; 
+		 int step = input.nextInt() ; 
+		 
+		 
+		 // creating the loop printing the f to c 
+		 for (int i = start; i <= end; i = i+step) {
+			
+			 int c = (i - 32) * (5/9);
+			 System.out.println(i+" "+c);
+			
+		}
 	}
 
 }
---
+++ src/BinnerySearch/Binneary_Search.java
@@ -0,0 +1,23 @@
+package BinnerySearch;
+
+public class Binneary_Search {
+	
+	// Creating the function of the liner search 
+	// taking the input from the user 
+	public static <T>T[] taking_input(){
+		
+		T[]arr = new ; 
+		return arr ; 
+	}
+
+	public static <T> T userinput(T data) {
+	  return data ; 
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println(userinput(65));
+	}
+
+}
