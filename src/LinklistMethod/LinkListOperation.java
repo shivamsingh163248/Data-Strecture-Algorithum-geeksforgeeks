cd6b6aeb781b22b16ae3133510d72319fd6f8d6f
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-17 10:30:46 +0530

    larning the linklist operaion

--- src/LinklistMethod/LinkListOperation.java
+++ src/LinklistMethod/LinkListOperation.java
@@ -11,11 +11,27 @@ public class LinkListOperation<T>{
 		// fist approach check the head is the null or not 
 		// creating the the condition for the checking 
 		if (head == null) {
-			
 			// create a node for the head 
 			head = new Node<T>(data);
+		}else {
+			
+			// create the copy of the node and then the chack k of the last node 
+			Node<T>copy_head = head ; 
+			// Iterate 
+			// uisng the loop using the while loop 
+			while (copy_head != null ) {
+				// find the last node 
+				copy_head = copy_head.Next ; 
+			}
+			// creating the new node and attached in the next 
+			Node<T>New_node = new Node<T>(data) ; 
+			copy_head.Next = New_node ; 
 		}
 		
+		
+		
 	}
+	
+	// creating the function for the iterating 
 
 }
