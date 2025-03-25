c6a26c0c2d684c3c9df8f4d8f0528bd66bc3369e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-25 19:36:53 +0530

    update

--- Basic_concept/Bank_operation/TestBank.java
+++ Basic_concept/Bank_operation/TestBank.java
@@ -4,52 +4,26 @@ public class TestBank {
 	
 	public static void main(String[] args) {
 		
-		   Bank sbi = new Bank("SBI");
-	        
-	        // First account
-	        sbi.new_account("Shivam", 200, "Nadihar", 345674);
-	        sbi.Print_account_Details();
-	        sbi.user_account_details(345674);
-	        System.out.println(sbi.total_account());
-	        sbi.user_account_details(3456774);
-	        sbi.deposit(345674, 34);
-	        sbi.user_account_details(345674);
-	        sbi.Withdraw(345674, 67);
-	        sbi.user_account_details(345674);
-	        sbi.Withdraw(345674, 200);
-	        sbi.user_account_details(345674);
-	        sbi.Withdraw(3456742, 67);
-	        sbi.deposit(3456743, 34);
-	        
-	        // Second account
-	        sbi.new_account("Amit", 500, "Delhi", 987654);
-	        sbi.Print_account_Details();
-	        sbi.user_account_details(987654);
-	        System.out.println(sbi.total_account());
-	        sbi.user_account_details(9876574);
-	        sbi.deposit(987654, 100);
-	        sbi.user_account_details(987654);
-	        sbi.Withdraw(987654, 150);
-	        sbi.user_account_details(987654);
-	        sbi.Withdraw(987654, 500);
-	        sbi.user_account_details(987654);
-	        sbi.Withdraw(9876542, 100);
-	        sbi.deposit(9876543, 50);
-	        
-	        // Third account
-	        sbi.new_account("Rahul", 300, "Mumbai", 123456);
-	        sbi.Print_account_Details();
-	        sbi.user_account_details(123456);
-	        System.out.println(sbi.total_account());
-	        sbi.user_account_details(1234576);
-	        sbi.deposit(123456, 75);
-	        sbi.user_account_details(123456);
-	        sbi.Withdraw(123456, 120);
-	        sbi.user_account_details(123456);
-	        sbi.Withdraw(123456, 300);
-	        sbi.user_account_details(123456);
-	        sbi.Withdraw(1234562, 80);
-	        sbi.deposit(1234563, 90);
+		// creating the object of sbi 
+		Bank sbi = new Bank("sbi") ; 
+		
+		// now create a account 
+		sbi.new_account("shivam", 200, "nadihar", 345674);
+		
+		// now print 
+		sbi.Print_account_Details();
+		sbi.user_account_details(345674);
+		sbi.total_account() ; 
+		System.out.println(sbi.total_account() );
+		sbi.user_account_details(3456774);
+		sbi.deposit(345674, 34);
+		sbi.user_account_details(345674);
+		sbi.Withdraw(345674, 67);
+		sbi.user_account_details(345674);
+		sbi.Withdraw(345674, 200);
+		sbi.user_account_details(345674);
+		sbi.Withdraw(3456742, 67);
+		sbi.deposit(3456743, 34);
 		
 	}
 
