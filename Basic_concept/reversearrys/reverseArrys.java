a8c56aa9c7104046e2822666da717f11f6c55031
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-03-29 18:42:11 +0530

    reversing arrys

---
+++ Basic_concept/reversearrys/reverseArrys.java
@@ -0,0 +1,66 @@
+package reversearrys;
+
+public class reverseArrys {
+	
+	// create the method 1 
+	public int[] reverse_arrays_1(int[]arr) {
+		
+		// using the two pointer 
+		// using the while loop method 
+		int start = 0  ; 
+		int end = arr.length -1 ; 
+		
+		// create a while loop 
+		while (start < end) {
+			// take a temp variable for the replacing 
+			int temp = arr[start] ; 
+			arr[start] = arr[end] ; 
+			arr[end] = temp ; 
+			
+			// now increment variable 
+			start++ ; 
+			end -- ; 
+			
+		}
+		
+		return arr ; 
+	}
+	
+	// creating the second method 
+	public int[] reverse_arrays_2(int[]arr) {
+		
+		// creating the new arrys 
+		int[]newarr = new int[arr.length] ; 
+		// creating the for loop stored in the second arrays 
+		for (int i = arr.length -1 ; i >= 0; i--) {
+			newarr[arr.length-1 - i] =  arr[i] ; 
+		}
+		return newarr ; 
+	}
+	
+	// creating the another method 
+	// using the recursion 
+	public int[] reverse_arrays(int[]arr) {
+		
+		 return reverse_arrays_2(arr, 0, arr.length -1 ) ; 
+	}
+	
+	
+    // creating the recursion 
+	public int[] reverse_arrays_2(int[]arr , int start , int end) {
+		
+		// creating the base case 
+		if (start<end) {
+			return  arr ; 
+		}
+		
+		int temp = arr[start] ; 
+		arr[start] = arr[end] ; 
+		arr[end] = temp ; 
+		
+		// now calling the function 
+	    return reverse_arrays_2(arr, start+1, end-1) ; 
+	    
+	}
+	
+}
