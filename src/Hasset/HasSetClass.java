c10a2aa319b2638ae1d4f6d8991fe951cde99d75
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-06-12 01:36:45 +0530

    update

--- src/Hasset/HasSetClass.java
+++ src/Hasset/HasSetClass.java
@@ -20,7 +20,26 @@ public class HasSetClass {
 	    // creating the second object one is 
 	    HasSet<Integer, String>map1 = new HasSet<Integer, String>() ; 
 	    HasSet<String, Integer>map2 = new HasSet<String, Integer>() ; 
+	    HasSet<String, String>map3 = new HasSet<String, String>() ; 
 	    // creating the type of the string and string 
+	    // check the size and check for the isEmpty or not 
+	    
+	    System.out.println(map1.insert(1, "shivam"));
+	    System.out.println(map1.insert(2, "itisha"));
+	    System.out.println(map1.insert(3, "lovely"));
+	    System.out.println(map1.insert(4, "shivam"));
+	    System.out.println(map1.insert(5, "itisha"));
+	    map1.getData();
+	    System.out.println(map1.getSize());
+	    System.out.println("shivam".hashCode());
+	    System.out.println(map3.isEmpty());
+	    System.out.println(map3.insert("class1", "shivam"));
+	    System.out.println(map3.insert("class1", "itisha"));
+	    System.out.println(map3.insert("class3", "lovely"));
+	    System.out.println(map3.insert("class1", "shivam"));
+	    System.out.println(map3.insert("class2", "itisha"));
+	    map3.getData();
+	    System.out.println(map3.getSize());
 	    System.out.println("shivam".hashCode());
 	}
 
