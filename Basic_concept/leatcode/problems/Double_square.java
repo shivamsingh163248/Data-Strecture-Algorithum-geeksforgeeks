c797cb640fd4f444ac0bff81d62dee2818ba5441
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-06-23 04:46:23 +0530

    update

--- Basic_concept/leatcode/problems/Double_square.java
+++ Basic_concept/leatcode/problems/Double_square.java
@@ -13,10 +13,22 @@ public static int[] sortedSquares(int[] nums) {
 	// now start the shorting 
 	// creating the two pointer 
 	int start = 0 ; 
-	int end = nums.length - 1 ; 
+	int end = nums.length - 1 ;
+	int index = nums.length -1 ; 
 	// now write the while loop for this operation 
-	 
-	
+	 while (start<= end) {
+		 // check the condition fist  
+		 if (nums[start] <= nums[end]) {
+			result[index] = nums[end] ; 
+			index -- ; 
+			end-- ; 
+		}
+		 if (nums[start] >= nums[end]) {
+			 result[index] = nums[start] ; 
+				index -- ; 
+				start++ ; 
+		}
+	}
 	return result ; 
     }
 
