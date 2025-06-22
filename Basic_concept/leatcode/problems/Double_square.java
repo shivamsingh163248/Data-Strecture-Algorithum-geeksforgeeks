5d21344ffde0254a5a34dfcd3dc5dc6c9b5af1a7
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-06-23 02:18:07 +0530

    update

--- Basic_concept/leatcode/problems/Double_square.java
+++ Basic_concept/leatcode/problems/Double_square.java
@@ -2,10 +2,21 @@ package leatcode.problems;
 
 public class Double_square {
 	
-public static int[] sortedSquares(int[] nums) {
-        
+public static int[] sortedSquares(int[] nums) {   
      // create a new arrays 
 	int result[] =  new int[nums.length] ; 
+	// creating the for loop the for the square then start the shorting algorithm 
+	for (int i = 0; i < nums.length; i++) {
+		nums[i] *= nums[i] ; 
+	}
+	
+	// now start the shorting 
+	// creating the two pointer 
+	int start = 0 ; 
+	int end = nums.length - 1 ; 
+	// now write the while loop for this operation 
+	 
+	
 	return result ; 
     }
 
