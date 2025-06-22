c21e6d749bfb3acf771ccc0220670477c6450b7a
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2025-06-22 14:40:06 +0530

    upate

---
+++ Basic_concept/leatcode/problems/Double_square.java
@@ -0,0 +1,17 @@
+package leatcode.problems;
+
+public class Double_square {
+	
+public static int[] sortedSquares(int[] nums) {
+        
+     // create a new arrays 
+	int result[] =  new int[nums.length] ; 
+	return result ; 
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
